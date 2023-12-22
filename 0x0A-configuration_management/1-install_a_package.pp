a61543e2da563659485361ba202cc791e625e07a
Your Name <you@example.com>
2023-12-22 12:54:56 -0800

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Install Flask
+# Install Flask using pip3 with the specified version
 package { 'Flask':
-	ensure   => '2.1.0',
-	provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
