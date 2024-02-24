ed9dcfb8bcdb68ac9d910f36b47ed56df730e4c0
Your Name <you@example.com>
2024-02-24 08:04:03 +0000

    h

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-ky_is_the_limit_not.pp set open file limit higher
+#0-the_sky_is_the_limit_not.pp set open file limit higher
 exec { 'set limit to 2000':
   path    => '/bin',
   command => "sed -i 's/15/2000/' /etc/default/nginx"
